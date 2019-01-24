6883f5b68914155a0ea6adcdb0d3bca4db3cda67
Yi Xin <angyixin@gmail.com>
2019-01-24 16:52:58 +0800

    some button stuff

--- js/app.js
+++ js/app.js
@@ -54,100 +54,23 @@ window.onload = function () {
           ],
       },
     },
-    // methods: {
-    //   createTodo(event) {
-    //     const textbox = event.target;
-    //     this.todos.push({ text: textbox.value, isDone: false });
-    //     textbox.value = '';
-    //   },
-    //   startEditing(todo) {
-    //     this.editing = todo;
-    //   },
-    // }
+    methods: {
+      createBookmark() {
+        var modal = document.getElementById('myModal');
+
+        console.log("asdas");
+        modal.style.display = "block";
+      },
+      closeModal() {
+        var modal = document.getElementById('myModal');
+
+        console.log("wewr");
+        modal.style.display = "none";
+      },
+      addObject() {
+        console.log("dsfdsf");
+      }
+    }
   });
 
 }
-
-
-var bookmarks1= {
-
-    "ACE": {
-      "desc" : "Apply leave, submit claims.",
-      "url"  : "https://portal.ace.gov.sg",
-      "category"  : "admin"
-    },
-    "iLearn": {
-      "desc" : "Search and apply for courses.",
-      "url"  : "https://portal.ace.gov.sg",
-      "category"  : "admin"
-    },
-    "EUSH": {
-      "desc" : "End User Self Help. Windows password reset.",
-      "url"  : "https://portal.ace.gov.sg",
-      "category"  : "admin"
-    },
-    "ACCESS": {
-      "desc"      : "Performance review. Peer Bonus. Ugly Interface.",
-      "url"       : "https://portal.ace.gov.sg",
-      "category"  : "admin"
-    },
-    "One Drive": {
-      "desc"      : "Shared Drive for files",
-      "url"       : "https://portal.ace.gov.sg",
-      "category"  : "productivity"
-    },
-    "EtherDocs": {
-      "desc"      : "Collaborative editing on documents",
-      "url"       : "https://portal.ace.gov.sg",
-      "category"  : "productivity"
-    },
-    "Form.gov.sg": {
-      "desc"       : "Create government forms and surveys in minutes.",
-      "url"        : "https://portal.ace.gov.sg",
-      "category"   : "productivity"
-    },
-    "EUSH": {
-      "desc"       : "Build a secure government website quickly.",
-      "Isomer"     : "https://portal.ace.gov.sg",
-      "category"   : "productivity"
-    },
-};
-
-var bookmarks= {
-  "admin": {
-      "ACE": {
-        "desc" : "Apply leave, submit claims.",
-        "url"  : "https://portal.ace.gov.sg",
-      },
-      "iLearn": {
-        "desc" : "Search and apply for courses.",
-        "url"  : "https://portal.ace.gov.sg",
-      },
-      "EUSH": {
-        "desc" : "End User Self Help. Windows password reset.",
-        "url"  : "https://portal.ace.gov.sg",
-      },
-      "ACCESS": {
-        "desc" : "Performance review. Peer Bonus. Ugly Interface.",
-        "url"  : "https://portal.ace.gov.sg",
-      }
-  },
-  "productivity": {
-      "One Drive": {
-        "desc" : "Shared Drive for files",
-        "url"  : "https://portal.ace.gov.sg",
-      },
-      "EtherDocs": {
-        "desc" : "Collaborative editing on documents",
-        "url"  : "https://portal.ace.gov.sg",
-      },
-      "Form.gov.sg": {
-        "desc" : "Create government forms and surveys in minutes.",
-        "url"  : "https://portal.ace.gov.sg",
-      },
-      "EUSH": {
-        "desc" : "Build a secure government website quickly.",
-        "Isomer.gov.sg"  : "https://portal.ace.gov.sg",
-      }
-  }
-};
